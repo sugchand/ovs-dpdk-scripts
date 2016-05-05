08f7a464a57e052826afc9ff4b175b0d9b914b30
Sugesh Chandran <sugeshchandran@gmail.com>
2016-05-05 17:35:17 +0100

    OVS DPDK testPMD application script to run inside the VM.
    The script for run simple testpmd l2 forwarding application inside the VM
    having two vhost-user ports.
    
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

---
+++ bash-scripts/vm-testpmd-script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# mount hugepages
+sysctl vm.nr_hugepages=256
+mkdir -p /dev/hugepages
+mount -t hugetlbfs hugetlbfs /dev/hugepages
+
+# mount shared directory
+
+# build and configure system for dpdk
+cd /root/ovdk/DPDK
+export CC=gcc
+export RTE_SDK=/root/ovdk/DPDK
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+
+#if enabled, build all of DPDK, if not build only the kernel module
+make uninstall
+make install T=x86_64-native-linuxapp-gcc
+
+modprobe uio
+rmmod igb_uio
+
+insmod x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+./tools/dpdk_nic_bind.py --status
+./tools/dpdk_nic_bind.py -b igb_uio 00:03.0 00:04.0
+
+# build and run 'test-pmd'
+cd /root/ovdk/DPDK/app/test-pmd
+make clean
+make
+./testpmd -c 0x3 -n 4 --socket-mem 128 -- --burst=64 -i --txqflags=0xf00
+set fwd mac_retry
+start
+
