49fb36ebc6af3a31c6781648f672e544b0580a0f
Sugesh Chandran <sugeshchandran@gmail.com>
2016-05-05 17:31:52 +0100

    Update phy-phy and phy-vm-phy scripts to use new DPDK configuration.
    DPDK configuration options such as core mask, memory and etc are not part
    of existing ovs-vswitchd command line options any more. This must be
    configured in the ovsdb before starting the ovs-vswitchd.
    
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

--- bash-scripts/phy2phy_manual.sh
+++ bash-scripts/phy2phy_manual.sh
@@ -21,8 +21,13 @@ function start_test {
 	sudo rm /usr/local/etc/openvswitch/conf.db
 	sudo $OVS_DIR/ovsdb/ovsdb-tool create /usr/local/etc/openvswitch/conf.db $OVS_DIR/vswitchd/vswitch.ovsschema
 
-	sudo $OVS_DIR/ovsdb/ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile &
-	sudo -E $OVS_DIR/vswitchd/ovs-vswitchd --dpdk -c 0x2 -n 4 --socket-mem=1024,1 -- --pidfile unix:/usr/local/var/run/openvswitch/db.sock --log-file &
+	sudo $OVS_DIR/ovsdb/ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach
+    sudo $OVS_DIR/utilities/ovs-vsctl --no-wait init
+    sudo $OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
+    sudo $OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-lcore-mask="0x4"
+    sudo $OVS_DIR/utilities/ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem="1024,0"
+
+	sudo -E $OVS_DIR/vswitchd/ovs-vswitchd --pidfile unix:/usr/local/var/run/openvswitch/db.sock --log-file --detach
 	sleep 22
 	sudo $OVS_DIR/utilities/ovs-vsctl --timeout 10 del-br br0
 	sudo $OVS_DIR/utilities/ovs-vsctl --timeout 10 add-br br0 -- set bridge br0 datapath_type=netdev
