943d2001f92f3c32d1acc6e72f11f559eb248f5d
Sugesh Chandran <sugeshchandran@gmail.com>
2017-05-24 09:30:47 +0100

    Adding powercycle details of new board.
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

--- bash-scripts/powercycle.sh
+++ bash-scripts/powercycle.sh
@@ -1,6 +1,9 @@
 
-#Powercycle for QAT machine.
+#Powercycle for QAT machine silpixa00389820.
 /home/sugeshch/perl/powerCycle.pl replacment_RR_C07_01 10
 
-#Powercycle for normal board.
+#Powercycle for normal board silpixa00389816.
 /home/sugeshch/perl/powerCycle.pl replacment_RR_C07_06 10 
+
+#Powercycle for silpixa00397990
+/home/sugeshch/perl/powerCycle.pl RR_C10_08 10
