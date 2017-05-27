8a494adfd1d10eede222a073e9afa0a6228c30fc
Sugesh Chandran <sugesh.chandran@intel.com>
2017-05-27 16:50:23 +0100

    Add DPDK version support.
    DPDK APIs paths and function are changing time to time for each release.
    The test scripts are based only on OVS release. It is impossible
    to specify what version of DPDK is using while running the scripts
    Now adding the support for specifying DPDK version.
    DPDK version can be specified in the environment file
    'DPDK_VER'. It can be 16.04, 16.07, ... By default the version
    is 16.11
    
    Signed-off-by: Sugesh Chandran <sugesh.chandran@intel.com>

--- bash-scripts/std_funcs.sh
+++ bash-scripts/std_funcs.sh
@@ -3,10 +3,42 @@
 # This file contains a place to put standard code that is common across several
 # test scripts. All the functions should be called std_...
 
+declare DPDK_IGB_UIO
+declare DPDK_BIND_TOOL
+
 # Load the ovs schema and start ovsdb.
 std_start_db() {
-	sudo rm /usr/local/etc/openvswitch/conf.db
-	sudo $OVS_DIR/ovsdb/ovsdb-tool create /usr/local/etc/openvswitch/conf.db $OVS_DIR/vswitchd/vswitch.ovsschema
+    sudo rm /usr/local/etc/openvswitch/conf.db
+    sudo $OVS_DIR/ovsdb/ovsdb-tool create /usr/local/etc/openvswitch/conf.db $OVS_DIR/vswitchd/vswitch.ovsschema
 
-	sudo $OVS_DIR/ovsdb/ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach
+    sudo $OVS_DIR/ovsdb/ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach
 }
+
+
+####################################
+#  DPDK specific functions         #
+####################################
+
+#Set environment specific for DPDK version
+set_dpdk_env() {
+    case "$DPDK_VER" in
+    16.04)
+        DPDK_IGB_UIO=$DPDK_DIR/$DPDK_TARGET/kmod/igb_uio.ko
+        DPDK_BIND_TOOL=$DPDK_DIR/tools/dpdk_nic_bind.py
+        ;;
+    16.07|16.11)
+        DPDK_IGB_UIO=$DPDK_DIR/$DPDK_TARGET/kmod/igb_uio.ko
+        DPDK_BIND_TOOL=$DPDK_DIR/tools/dpdk-devbind.py
+        ;;
+    17.02|17.05)
+        DPDK_IGB_UIO=$DPDK_DIR/$DPDK_TARGET/kmod/igb_uio.ko
+        DPDK_BIND_TOOL=$DPDK_DIR/usertools/dpdk-devbind.py
+        ;;
+    *)
+        echo "DPDK version is not specified, Use default version settings"
+        DPDK_IGB_UIO=$DPDK_DIR/$DPDK_TARGET/kmod/igb_uio.ko
+        DPDK_BIND_TOOL=$DPDK_DIR/tools/dpdk-devbind.py
+        ;;
+    esac
+}
+
