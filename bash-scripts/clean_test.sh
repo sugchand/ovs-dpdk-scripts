19577d2dc5afedc06faa3e1f75129ee35d30ad44
Sugesh Chandran <sugeshchandran@gmail.com>
2017-10-20 11:41:25 +0100

    Update the clean script to do the cleaning in graceful manner.
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

--- bash-scripts/clean_test.sh
+++ bash-scripts/clean_test.sh
@@ -1,17 +1,23 @@
 #!/bin/bash -x
 
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${SRC_DIR}/banner.sh
+. ${SRC_DIR}/std_funcs.sh
 
 # Variables #
 HUGE_DIR=/dev/hugepages
 
 function clean {
+    set_dpdk_env
     sudo $OVS_DIR/utilities/ovs-appctl -t ovs-vswitchd exit
     sudo $OVS_DIR/utilities/ovs-appctl -t ovsdb-server exit
+    sudo $OVS_DIR//utilities/ovs-dpctl del-dp ovs-system
+    sudo modprobe -r openvswitch
     sleep 1
     sudo umount $HUGE_DIR
-    #sudo $DPDK_DIR/tools/dpdk-devbind.py --unbind $DPDK_PCI1 $DPDK_PCI2 $DPDK_PCI3 $DPDK_PCI4
+    sudo $DPDK_BIND_TOOL --unbind $DPDK_PCI1 $DPDK_PCI2 $DPDK_PCI3 $DPDK_PCI4
     sudo modprobe $KERNEL_NIC_DRV
-    sudo $DPDK_DIR/tools/dpdk-devbind.py --bind=$KERNEL_NIC_DRV $DPDK_PCI1 $DPDK_PCI2
+    sudo $DPDK_BIND_TOOL --bind=$KERNEL_NIC_DRV $DPDK_PCI1 $DPDK_PCI2
     sudo pkill -9 ovs-vswitchd
     sudo pkill -9 ovs-vsctl
     sudo pkill -9 qemu-system-x86_64*
