e90fa7b01c3155f9df94b3cd9fcec26eec467613
Billy O'Mahony <billy.o.mahony@intel.com>
2018-03-08 15:03:52 +0000

    std_funcs: create std funcs for clean-up

--- bash-scripts/std_funcs.sh
+++ bash-scripts/std_funcs.sh
@@ -9,11 +9,51 @@ declare DPDK_BIND_TOOL
 # Load the ovs schema and start ovsdb.
 std_start_db() {
     sudo rm /usr/local/etc/openvswitch/conf.db
-    sudo $OVS_DIR/ovsdb/ovsdb-tool create /usr/local/etc/openvswitch/conf.db $OVS_DIR/vswitchd/vswitch.ovsschema
-
-    sudo $OVS_DIR/ovsdb/ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach
+    sudo $OVS_DIR/ovsdb/ovsdb-tool \
+        create /usr/local/etc/openvswitch/conf.db \
+        $OVS_DIR/vswitchd/vswitch.ovsschema
+    sudo $OVS_DIR/ovsdb/ovsdb-server \
+        --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+        --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+        --pidfile --detach
 }
 
+function std_stop_db() {
+    sudo $OVS_DIR/utilities/ovs-appctl --timeout=3 -t ovsdb-server exit
+    sleep 1
+    sudo pkill -9 ovsdb-server
+}
+
+function std_stop_ovs() {
+    sudo $OVS_DIR/utilities/ovs-appctl --timeout=3 -t ovs-vswitchd exit
+    sleep 1
+    sudo rm -rf /usr/local/var/log/openvswitch/*
+    sudo rm -rf /usr/local/var/run/openvswitch/*
+    sudo pkill -9 ovs-vsctl
+    sudo pkill -9 pmd*
+    sudo pkill -9 ovs-vswitchd
+}
+
+function std_umount() {
+    sudo umount $HUGE_DIR
+}
+
+function std_bind_kernel() {
+    sudo $DPDK_DIR/tools/dpdk-devbind.py --unbind $DPDK_PCI1 $DPDK_PCI2 $DPDK_PCI3 $DPDK_PCI4
+    sudo modprobe $KERNEL_NIC_DRV
+    sudo $DPDK_BIND_TOOL --bind=$KERNEL_NIC_DRV $DPDK_PCI1 $DPDK_PCI2
+}
+
+function std_stop_vms() {
+    sudo pkill -9 qemu-system-x86_64*
+}
+
+function std_clean {
+    std_stop_vms
+    std_stop_ovs
+    std_stop_db
+    std_umount
+}
 
 ####################################
 #  DPDK specific functions         #
